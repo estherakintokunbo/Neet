bc450080c0011a118ff7b83f9ddf907237e1396c
Akintokunbo Esther Ayomide <estherakintokunbo1@gmail.com>
2024-01-07 22:01:25 +0000

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,14 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript{
+    dependencies{
+        //classpath("com.google.dagger:hilt-android-gradle-plugin:2.47")
+    }
+}
+
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
-
-android {
-    namespace = "com.example.neet"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.neet"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    //Libraries
-    implementation("de.hdodenhof:circleimageview:3.0.0")
-    implementation("com.google.android.material:material:1.0.0")
-
-}
-
